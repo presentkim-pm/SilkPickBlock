a169d046f8522df40cab2254b3825ab1b003221d
PresentKim <debe3721@gmail.com>
2022-02-11 20:55:28 +0900

    :tada: Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<div align="center">
+  <h1><strong> refSilkPickBlock </strong></h1>
+  <h1><img src="https://views.whatilearened.today/views/github/refteams/refSilkPickBlock.svg?style=for-the-badge" height="24" align="right"></h1><br>  
+  <br>  
+  <img src="https://img.shields.io/github/stars/refteams/refSilkPickBlock?style=for-the-badge" height="24">  
+  <img src="https://img.shields.io/github/license/refteams/refSilkPickBlock.svg?style=for-the-badge" height="24">
+  <h2><strong> Pick original block with sneaking like silk-touch drops </strong></h2>
+</div>
---
+++ src/ref/product/silkpickblock/Main.php
@@ -0,0 +1,86 @@
+<?php
+
+/**            __   _____
+ *  _ __ ___ / _| |_   _|__  __ _ _ __ ___
+ * | '__/ _ \ |_    | |/ _ \/ _` | '_ ` _ \
+ * | | |  __/  _|   | |  __/ (_| | | | | | |
+ * |_|  \___|_|     |_|\___|\__,_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  ref-team
+ * @link    https://github.com/refteams
+ *
+ *  &   ／l、
+ *    （ﾟ､ ｡ ７
+ *   　\、ﾞ ~ヽ   *
+ *   　じしf_, )ノ
+ *
+ * @noinspection PhpUnused
+ */
+
+declare(strict_types=1);
+
+namespace ref\product\silkpickblock;
+
+use pocketmine\event\EventPriority;
+use pocketmine\event\player\PlayerBlockPickEvent;
+use pocketmine\plugin\PluginBase;
+
+use function count;
+use function is_dir;
+use function rmdir;
+use function scandir;
+
+class Main extends PluginBase{
+    /** @throws \ReflectionException */
+    public function onEnable() : void{
+        /**
+         * This is a plugin that does not use data folders.
+         * Delete the unnecessary data folder of this plugin for users.
+         */
+        $dataFolder = $this->getDataFolder();
+        if(is_dir($dataFolder) && count(scandir($dataFolder)) <= 2){
+            rmdir($dataFolder);
+        }
+
+        $this->getServer()->getPluginManager()->registerEvent(PlayerBlockPickEvent::class, static function(PlayerBlockPickEvent $event) : void{
+            $player = $event->getPlayer();
+            if(!$player->isSneaking()){
+                return;
+            }
+
+            $inventory = $player->getInventory();
+            $item = $event->getBlock()->getSilkTouchDrops($inventory->getItemInHand())[0] ?? null;
+            if($item === null || $event->getResultItem()->equals($item, true, true)){
+                return;
+            }
+            $existingSlot = $inventory->first($item);
+            if($existingSlot === -1 && $player->hasFiniteResources()){
+                return;
+            }
+
+            if($existingSlot !== -1){
+                if($existingSlot < $inventory->getHotbarSize()){
+                    $inventory->setHeldItemIndex($existingSlot);
+                }else{
+                    $inventory->swap($inventory->getHeldItemIndex(), $existingSlot);
+                }
+            }else{
+                $firstEmpty = $inventory->firstEmpty();
+                if($firstEmpty === -1){ //full inventory
+                    $inventory->setItemInHand($item);
+                }elseif($firstEmpty < $inventory->getHotbarSize()){
+                    $inventory->setItem($firstEmpty, $item);
+                    $inventory->setHeldItemIndex($firstEmpty);
+                }else{
+                    $inventory->swap($inventory->getHeldItemIndex(), $firstEmpty);
+                    $inventory->setItemInHand($item);
+                }
+            }
+        }, EventPriority::LOWEST, $this);
+    }
+}
